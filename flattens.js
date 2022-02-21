265c66ddd49a8963e54b11350cfab8a93bec2670
Amram1997 <amramananyan9@gmail.com>
2022-02-21 11:02:13 +0400

    fix: flaten Array function

--- flattens.js
+++ flattens.js
@@ -1,10 +1,12 @@
 "use strict";
-function flattensArray(arr, arr1 = []) {
-  return arr.reduce(function (prev, curent) {
-    if (typeof curent !== "object") {
-      arr1.push(curent);
-      return arr1;
-    }
-  }, []);
+function flattensArray(arr) {
+  if (Array.isArray(arr)) {
+    return arr.reduce((prev, curent) => {
+      return prev.concat(flattensArray(curent));
+    }, []);
+  } else {
+    return arr;
+  }
 }
+console.log(flattensArray([14, [1, [[[3, []]], 1], 0]]));
 console.log(flattensArray([1, [3, 4, [1, 2]], 10]));
